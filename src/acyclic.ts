442facf87224b3590468edf2d01e1b908727ef85
Russell Brand <brand@responsible.com>
2025-02-17 19:48:35 -0800

    passed test

--- src/acyclic.ts
+++ src/acyclic.ts
@@ -3,22 +3,38 @@
  
  */
 export type Node = string;
+export type EdgeGroup = [Node, Node[]];
 
 export type Tree = Map<Node, Node[]>;
+
+export function edge_group_children(edge_group: EdgeGroup): Node[] {
+  const [_parent, children] = edge_group;
+  return children;
+}
+
+export function parents(node: Node, tree: Tree): Node[] {
+  const parents = [];
+  for (const [parent, children] of tree) {
+    if (children.includes(node)) {
+      parents.push(parent);
+    }
+  }
+  return parents;
+}
 
 /***
  * make_tree does no validation of the input. It is the responsibility of the caller to ensure that the input is valid.
  * ***/
-export function make_tree(nodes: Node[], connections: [Node, Node][]): Tree {
+export function make_tree(root: Node, list_parent_child: [Node, Node][]): Tree {
   const edges = new Map<Node, Node[]>();
+  edges.set(root, []);
+  //   for (const node of nodes) {
+  //     if (!edges.has(node)) {
+  //       edges.set(node, []);
+  //     }
+  //   }
 
-  for (const node of nodes) {
-    if (!edges.has(node)) {
-      edges.set(node, []);
-    }
-  }
-
-  for (const [from, to] of connections) {
+  for (const [from, to] of list_parent_child) {
     if (!edges.has(from)) {
       edges.set(from, []);
     }
@@ -39,6 +55,12 @@ export function isValid(tree: Tree): boolean {
   if (tree_nodes(tree).length === 0) {
     return false;
   }
+  const [root, _] = tree;
+  const [root_node, _children_of_root] = root;
+  if (parents(root_node, tree).length !== 0) {
+    return false;
+  }
+
   return true;
 }
 export function tree_nodes(tree: Tree): Node[] {
--- src/the.test.ts
+++ src/the.test.ts
@@ -3,24 +3,24 @@ import { make_tree, root, Tree } from "./acyclic";
 import { isValid } from "./acyclic";
 
 const ROOT = "root";
-let INVALID_EMPTY_TREE: Tree;
+
 let SINGELON_TREE: Tree;
+let MISPLACED_ROOT_TREE: Tree;
 
 // let alice = 1;
 
 describe("Tree test suite", () => {
   beforeEach(() => {
-    INVALID_EMPTY_TREE = make_tree([], []);
-    SINGELON_TREE = make_tree([ROOT], []);
+    SINGELON_TREE = make_tree(ROOT, []);
+    MISPLACED_ROOT_TREE = make_tree(ROOT, [["super_root", ROOT]]);
   });
   describe("isValid function", () => {
     it("trees with just a root node are valid", () => {
       expect(isValid(SINGELON_TREE)).toBe(true);
       expect(root(SINGELON_TREE)).toBe(ROOT);
     });
-
-    it("trees must have root nodes", () => {
-      expect(isValid(INVALID_EMPTY_TREE)).toBe(false);
+    it("root cannot have parents", () => {
+      expect(isValid(MISPLACED_ROOT_TREE)).toBe(false);
     });
   });
 });
